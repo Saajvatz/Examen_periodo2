b08c8cd9694cbda68386dda744ce1df8c259720e
Saajvatz <ajvatzalejandro@gmail.com>
2024-07-11 12:01:24 -0600

    primera version

---
+++ header/header.js
@@ -0,0 +1,9 @@
+let div_header = document.querySelector(".header")
+
+
+
+div_header.innerHTML = `
+
+<h1 class="usuario">Saajvatz@</h1>
+<div class="Cuadrado"> <div class="repositorio"> <nav> <a href="https://github.com/Saajvatz/Examen_periodo2">repositorio</a></nav></div></div>`;
+
---
+++ secction/secction.js
@@ -0,0 +1,12 @@
+let section1_DOM = document.querySelector(".section1");
+
+section1_DOM.innerHTML =`
+
+<div class="papi">
+
+<div class="cuadro1"></div>
+<div class="cuadro2"></div>
+<div class="cuadro3"></div>
+
+</div>
+`;
